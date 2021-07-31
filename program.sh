b8d49f011261dda86292b39fe4dfdd32dfb2f6f4
deathstar <deathstar@program>
2021-08-01 00:39:49 +0300

    i do have git history, i remove no-history push from program.sh

--- program.sh
+++ program.sh
@@ -48,16 +48,4 @@ j-package(){
   
 }
 
-push(){
-  ORIGIN=$(git remote get-url origin)
-  rm -rf .git
-  git init -b main
-  git remote add origin $ORIGIN
-  git config --local include.path ../.gitconfig
-  git add .
-  git commit -m "i am deathstar program"
-  git push -f -u origin main
-}
-
-
 "$@"
