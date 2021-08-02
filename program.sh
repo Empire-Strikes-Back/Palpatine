e9880d38a67dcdd6cd282d14a928f8edb49e3046
deathstar <deathstar@program>
2021-08-02 22:44:20 +0300

    i use new commit hash of deps-repl

--- program.sh
+++ program.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
 repl(){
-  clj -M:repl
+  clj \
+    -X:repl deps-repl.core/process \
+    :main-ns get-to-the-ship.main \
+    :port 7788 \
+    :host '"0.0.0.0"' \
+    :repl? true \
+    :nrepl? false
 }
-
 
 main(){
   clojure -M:main
